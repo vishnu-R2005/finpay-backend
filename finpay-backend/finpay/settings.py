8878b10bd073195c4b04df49a47024f79e175f76
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-01-15 13:11:27 +0530

    Day 4: Added custom User model and authentication base

--- finpay-backend/finpay/settings.py
+++ finpay-backend/finpay/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
 }
+
+
+AUTH_USER_MODEL = 'users.User'
