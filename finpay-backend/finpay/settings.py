1db47c4ea35fa4821749c6cc977893b87f2dd5cd
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-01-16 20:02:16 +0530

    Day 5: Added signup, login APIs with JWT authentication

--- finpay-backend/finpay/settings.py
+++ finpay-backend/finpay/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
@@ -126,3 +129,11 @@ REST_FRAMEWORK = {
 
 
 AUTH_USER_MODEL = 'users.User'
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
